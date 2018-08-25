23b1cc2f83610e6fe522025b0ea8eb7e797d64ed
Dmitry <dgdagadin87@mail.ru>
2018-08-26 00:35:22 +0300

    Map was added

--- resources/js/Application.jsx
+++ resources/js/Application.jsx
@@ -2,4 +2,12 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/promise';
 
-alert('Initialized');
+import ymaps from 'ymaps';
+
+ymaps.load().then(maps => {
+    const map = new maps.Map('map-container', {
+        center: [55.76, 37.64],
+        zoom: 18
+    });
+})
+.catch(error => console.log('Failed to load Yandex Maps', error));
